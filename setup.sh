d126347e88457edf9da82d8f9168509a10195e37
Arkadiy-Garber <arkg@udel.edu>
2019-11-04 14:27:23 -0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -9,14 +9,14 @@ printf "\n    ${GREEN}Setting up conda environment...${NC}\n\n"
 
 chmod +x pseudofinder.py
 
-## adding conda channels
-conda config --add channels defaults 2> /dev/null
-conda config --add channels bioconda 2> /dev/null
-conda config --add channels conda-forge 2> /dev/null
-conda config --add channels au-eoed 2> /dev/null
+## adding conda channels (THIS PART DEPRACATED)
+# conda config --add channels defaults 2> /dev/null
+# conda config --add channels bioconda 2> /dev/null
+# conda config --add channels conda-forge 2> /dev/null
+# conda config --add channels au-eoed 2> /dev/null
 
 ## creating GToTree environment and installing dependencies
-conda create -n pseudo blast diamond pal2nal muscle paml prodigal biopython plotly pandas numpy reportlab --yes
+conda create -n gtotree -c conda-forge -c bioconda -c defaults biopython hmmer=3.2.1 muscle=3.8.1551 trimal=1.4.1 fasttree=2.1.10 iqtree=1.6.9 prodigal=2.6.3 taxonkit=0.3.0 parallel=20190722 --yes
 
 ## activating environment
 source activate pseudo
