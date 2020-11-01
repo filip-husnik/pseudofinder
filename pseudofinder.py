865d9a19e17e9f7ec8177e7a4d4e55ac5a821b40
Arkadiy-Garber <arkg@udel.edu>
2020-11-01 15:41:26 -0500

    Update pseudofinder.py

--- pseudofinder.py
+++ pseudofinder.py
@@ -14,7 +14,7 @@ __version__ = "0.11"
 __maintainer__ = "Filip Husnik"
 __email__ = "filip.husnik@gmail.com"
 
-errorMessage = "Options: " + common.bold("pseudofinder.py [ annotate | reannotate | visualize | dnds | test | help ]\n")
+errorMessage = "Options: " + common.bold("pseudofinder.py [ annotate | reannotate | visualize | selection | test | help ]\n")
 
 try:
     argv[1]
@@ -28,8 +28,8 @@ elif argv[1] == "reannotate":
     reannotate.main()
 elif argv[1] == "visualize":
     visualize.main()
-elif argv[1] == "dnds":
-    dnds.main()
+elif argv[1] == "selection":
+    selection.main()
 elif argv[1] == "test":
     pseudofinder_test.main()
 elif argv[1] == "help":
@@ -37,7 +37,7 @@ elif argv[1] == "help":
                  "\tpseudofinder.py reannotate: Begins the annotate pipeline post-BLAST.\n"
                  "\tpseudofinder.py visualize: Generates a 3D plot to visualize different combinations of "
                  "settings.\n"
-                 "\tpseudofinder.py dnds: dN/dS analysis via pairwise comparison against a reference genome. "
+                 "\tpseudofinder.py selection: dN/dS analysis via pairwise comparison against a reference genome. "
                  "Pseudogenes inferred from relaxed selection.\n"
                  "\tpseudofinder.py test: Runs all commands on a test dataset and checks that the outputs "
                  "are as expected.\n")
