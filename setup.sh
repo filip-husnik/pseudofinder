c49333a86d57592221edab5118a3c7093a180750
Mitch Syberg-Olsen <mitchsybergolsen@alumni.ubc.ca>
2022-03-13 11:10:38 -0400

    specify python version in setup.sh

--- setup.sh
+++ setup.sh
@@ -9,14 +9,27 @@ printf "\n    ${GREEN}Setting up conda environment...${NC}\n\n"
 
 chmod +x pseudofinder.py
 
-## adding conda channels (THIS PART DEPRACATED)
-# conda config --add channels defaults 2> /dev/null
-# conda config --add channels bioconda 2> /dev/null
-# conda config --add channels conda-forge 2> /dev/null
-# conda config --add channels au-eoed 2> /dev/null
-
-## creating GToTree environment and installing dependencies
-conda create -n pseudofinder -c conda-forge -c bioconda -c defaults biopython blast diamond pal2nal paml plotly pandas numpy reportlab hmmer=3.2.1 muscle=3.8.1551 prodigal=2.6.3 parallel=20190722 --yes
+## creating environment and installing dependencies
+conda create \
+-n pseudofinder \
+-c conda-forge \
+-c bioconda \
+-c defaults \
+python=3.10.2 \
+biopython=1.79 \
+blast=2.12.0 \
+diamond=2.0.14 \
+pal2nal=14.1 \
+paml=4.9 \
+plotly=5.6.0 \
+pandas=1.4.1 \
+numpy=1.22.2 \
+reportlab=3.5.68 \
+hmmer=3.2.1 \
+muscle=3.8.1551 \
+prodigal=2.6.3 \
+parallel=20190722 \
+--yes # Do not ask for confirmation
 
 ## activating environment
 source activate pseudofinder
@@ -27,13 +40,11 @@ mkdir -p ${CONDA_PREFIX}/etc/conda/activate.d
 ## adding codeml-2.ctl file path:
 echo '#!/bin/sh'" \
 
-
 export PATH=\"$(pwd):"'$PATH'\"" \
 
 export ctl=\"$(pwd)/codeml-2.ctl\"" >> ${CONDA_PREFIX}/etc/conda/activate.d/env_vars.sh
 
 # re-activating environment so variable and PATH changes take effect
 source activate pseudofinder
-
 
 printf "\n        ${GREEN}DONE!${NC}\n\n"
